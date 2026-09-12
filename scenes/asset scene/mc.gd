77f07a7b164f7a0cf73308d3e1b0d6363236f268
Kavya8989-core <ks2371008@gmail.com>
2026-09-12 19:08:47 +0530

    fixed attack script

--- scenes/asset scene/mc.gd
+++ scenes/asset scene/mc.gd
@@ -3,24 +3,33 @@ extends CharacterBody2D
 @onready var animatiedsprite2d = $AnimatedSprite2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var is_attacking := false
 
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("left", "right")
-
-	if direction:
-		velocity.x = direction * SPEED
+	if is_attacking == true:
+		velocity.x = 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 
 	process_animation(direction)
 
 
-func process_animation( direction: float) -> void:
-	if Input.is_action_pressed("attack"):
+func process_animation(direction: float) -> void:
+	if Input.is_action_just_pressed("attack"):
+		is_attacking = true
 		animatiedsprite2d.play("attack1 right")
+		await animatiedsprite2d.animation_finished
+		is_attacking = false
+	if is_attacking:
+		return 
+		
 	elif direction > 0:
 		animatiedsprite2d.flip_h = false
 		animatiedsprite2d.play("walk right")
@@ -28,5 +37,6 @@ func process_animation( direction: float) -> void:
 	elif direction < 0:
 		animatiedsprite2d.flip_h = true
 		animatiedsprite2d.play("walk right")
-	elif direction == 0:
+		
+	else:
 		animatiedsprite2d.play("idle right")
