3bb8169edba5c04920fad7347b85a2780c86c0a0
Kavya8989-core <ks2371008@gmail.com>
2026-09-12 19:19:25 +0530

    added jumping script

--- scenes/asset scene/mc.gd
+++ scenes/asset scene/mc.gd
@@ -4,10 +4,14 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var is_attacking := false
-
+var is_jumping := false
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("left", "right")
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_VELOCITY
 	if is_attacking == true:
 		velocity.x = 0
 	else:
@@ -29,7 +33,13 @@ func process_animation(direction: float) -> void:
 		is_attacking = false
 	if is_attacking:
 		return 
-		
+	if Input.is_action_just_pressed("jump"):
+		is_jumping = true
+		animatiedsprite2d.play("jump right")
+		await animatiedsprite2d.animation_finished
+		is_jumping = false
+	if is_jumping :
+		return
 	elif direction > 0:
 		animatiedsprite2d.flip_h = false
 		animatiedsprite2d.play("walk right")
@@ -37,6 +47,5 @@ func process_animation(direction: float) -> void:
 	elif direction < 0:
 		animatiedsprite2d.flip_h = true
 		animatiedsprite2d.play("walk right")
-		
 	else:
 		animatiedsprite2d.play("idle right")
