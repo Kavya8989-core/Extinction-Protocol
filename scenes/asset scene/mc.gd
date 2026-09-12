3588e609bb633ffe51d30ca1eb23c54a87474bf2
Kavya8989-core <ks2371008@gmail.com>
2026-09-12 18:48:53 +0530

    added basic movements script

--- scenes/asset scene/mc.gd
+++ scenes/asset scene/mc.gd
@@ -1,25 +1,30 @@
 extends CharacterBody2D
 
-
+@onready var animatiedsprite2d = $AnimatedSprite2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	var direction := Input.get_axis("left", "right")
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("right", "left")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	process_animation(direction)
+
+
+func process_animation( direction: float) -> void:
+	if direction > 0:
+		animatiedsprite2d.flip_h = false
+		animatiedsprite2d.play("walk right")
+		
+	elif direction < 0:
+		animatiedsprite2d.flip_h = true
+		animatiedsprite2d.play("walk right")
+	else:
+		animatiedsprite2d.play("idle right")
